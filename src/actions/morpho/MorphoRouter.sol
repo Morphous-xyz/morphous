d6f77627b7be4eb5974255cbcba61177936c9e1e
MerlinEgalite <egalite.merlin@gmail.com>
2022-11-13 13:34:09 +0100

    refactor: match naming

--- src/actions/morpho/MorphoRouter.sol
+++ src/actions/morpho/MorphoRouter.sol
@@ -2,10 +2,10 @@
 pragma solidity 0.8.17;
 
 import {Augustus} from "src/actions/paraswap/Augustus.sol";
-import {MorphoBorrow} from "src/actions/morpho/MorphoBorrow.sol";
+import {MorphoBorrowRepay} from "src/actions/morpho/MorphoBorrowRepay.sol";
 import {MorphoClaimRewards} from "src/actions/morpho/MorphoClaimRewards.sol";
 import {MorphoSupplyWithdraw} from "src/actions/morpho/MorphoSupplyWithdraw.sol";
 
 /// @notice Morpho Router.
 /// @author @Mutative_
-abstract contract MorphoRouter is Augustus, MorphoBorrow, MorphoClaimRewards, MorphoSupplyWithdraw {}
+abstract contract MorphoRouter is Augustus, MorphoBorrowRepay, MorphoClaimRewards, MorphoSupplyWithdraw {}
