41bef0fa0937076a80aeedc8a62e94180b81c8cd
Warren <99427649+kobe-eth@users.noreply.github.com>
2022-12-19 14:59:37 +0100

    release: deploy script

--- script/Deploy.sol
+++ script/Deploy.sol
@@ -1,13 +1,24 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.8.17;
 
+import "src/Neo.sol";
 import "src/Morphous.sol";
+import "src/actions/flashloan/FL.sol";
+
 import "forge-std/Script.sol";
 
 contract DeployScript is Script {
+    FL internal fl;
+    Neo internal neo;
+    Morphous internal morphous;
+
     function run() public {
         vm.startBroadcast();
-        console.log("Hello World");
+
+        morphous = new Morphous();
+        fl = new FL(address(morphous));
+        neo = new Neo(address(morphous), address(fl));
+
         vm.stopBroadcast();
     }
 }
